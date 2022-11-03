6dc2bf6469bca76ccad7181c1a1d47c28d45ae79
ychmdev <cryptoman0702@gmail.com>
2022-11-03 18:43:37 +1000

    fix issue for vercel deployment

--- next.config.js
+++ next.config.js
@@ -6,24 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const withTM = require('next-transpile-modules')(['@pancakeswap/uikit'])
 
 const sentryWebpackPluginOptions =
-  process.env.VERCEL_ENV === 'production'
-    ? {
-        // Additional config options for the Sentry Webpack plugin. Keep in mind that
-        // the following options are set automatically, and overriding them is not
-        // recommended:
-        //   release, url, org, project, authToken, configFile, stripPrefix,
-        //   urlPrefix, include, ignore
-        silent: false, // Logging when deploying to check if there is any problem
-        validate: false,
-        // Mark the release as Production
-        // https://github.com/getsentry/sentry-webpack-plugin/blob/master/src/index.js#L522
-        deploy: {
-          env: process.env.VERCEL_ENV,
-        },
-        // For all available options, see:
-        // https://github.com/getsentry/sentry-webpack-plugin#options.
-      }
-    : {
+      {
         validate: false,
         silent: true, // Suppresses all logs
         dryRun: !process.env.SENTRY_AUTH_TOKEN,
